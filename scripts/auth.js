07438685af9667c67b8dd218e99507e7896922ed
tymoschuk <tymoschuk@A34COMP8>
2019-01-21 17:27:18 +0200

    Change to jquery valodation

--- scripts/auth.js
+++ scripts/auth.js
@@ -2,16 +2,42 @@ $(function () {
     $('#password').val(window.localStorage.getItem("Password"));
     $('#email').val(window.localStorage.getItem("Login"));
     M.updateTextFields();  
-})
+    $('#auth_form').validate({
+      rules: {
+        first_name: 'required',
+        last_name: 'required',
+        email: {
+          required: true,
+          email: true,
+        },
+        password: {
+          required: true,
+          minlength: 4,
+        }
+       
+        
+      },
+      messages: {
+       
+        email: 'Enter a valid email',
+        password: {
+          minlength: 'Password must be at least 4 characters long'
+        }
 
-function Send() {
-    let login=$('#email').val();
-    let pass=$('#password').val();
-
-    
-
-
-    
-    window.location.href = "index.html";
+      },
+      submitHandler: function (form) {
+        form.submit();
+        window.location.href = "index.html";
+  
       }
+    });
+  })
+
+
+    
+
+
+    
+
+    
     
--- scripts/reg.js
+++ scripts/reg.js
@@ -1,21 +1,49 @@
-function Send() {
-let login=$('#email').val();
-let pass=$('#password').val();
-let cpass=$('#cpassword').val();
 
-if(pass!==cpass)
-{
-   
-    $( "div" ).data( "error" ) === "wrong";
-   
-}
 
-window.localStorage.setItem("Login",login);
-window.localStorage.setItem("Password",pass);
+$(function () {
+    
+    $('#reg_form').validate({
+      rules: {
+        first_name: 'required',
+        last_name: 'required',
+        email: {
+          required: true,
+          email: true,
+        },
+        password: {
+          required: true,
+          minlength: 4,
+        },
+        cpassword: {
+            equalTo: "#password"
+        },
+        Birthdate:{
+            required: true,
+            date:true,
+        }
 
-window.location.href = "Authorization.html";
-  }
+        
+      },
+      messages: {
+        first_name: 'This field is required',
+        last_name: 'This field is required',
+        email: 'Enter a valid email',
+        password: {
+          minlength: 'Password must be at least 4 characters long'
+        },
+        cpassword: 'Password fields must be equal',
+        Birthdate:'This field is required'
 
+      },
+      submitHandler: function (form) {
+        form.submit();
+        window.localStorage.setItem("Login",$('email').val());
+        window.localStorage.setItem("Password",$('password').val());
+        window.location.href = "Authorization.html";
+  
+      }
+    });
+  })
 
 document.addEventListener('DOMContentLoaded', function () {
     var options = {

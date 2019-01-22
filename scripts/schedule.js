781129ba806adbec0475d37eed3db6619139e347
tymoschuk <tymoschuk@A34COMP8>
2019-01-22 17:43:56 +0200

    add creatinf schedule table

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -2,5 +2,21 @@
 
 $(function () {
 
-    $('#table_body').html("<tr><td>3434</td><td>3434</td><td>3434</td><td>3434</td><td>3434</td></tr>");
+    $.ajax(
+        {
+            url:"http://localhost:54617/Services.svc/Airoport/API/Data/GetAllFlight",
+            type: "GET",
+            success: function (data){
+
+
+                
+               data.GetAllFlightResult.forEach(element => {
+               
+                var realTime=moment(moment(element.Date).toDate(),'llll');
+                $('#table_body').html(`<tr><td>${realTime}</td><td>${element.Plen}</td><td>${element.From}</td><td>${element.To}</td><td>${element.Count}</td></tr>`);
+               });
+            }
+        }
+    )
+    
 })

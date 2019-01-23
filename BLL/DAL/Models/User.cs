88337613f20a7a85fc6ca4ce04c94bc49d2a8d58
istep.andriy <istep.andriy@gmail.com>
2019-01-23 21:23:20 +0200

    bug fixed

--- BLL/DAL/Models/User.cs
+++ BLL/DAL/Models/User.cs
@@ -14,7 +14,7 @@ namespace DAL
         public int ID { get; set; }
         public string LName { get; set; }
         public string FName { get; set; }
-        public DateTime BirthDate { get; set; }
+        public string BirthDate { get; set; }
         public string Email { get; set; }
         public bool Gender { get; set; }
         public string  Card { get; set; }

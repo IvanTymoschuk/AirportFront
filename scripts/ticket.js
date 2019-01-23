319b5fbe526544c066bf07a962c023805ece1211
Ivan Tymoschuk <ivantymo99@gmail.com>
2019-01-23 13:59:42 +0200

    added modal to ticket order

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -1,3 +1,25 @@
-$(document).ready(function(){
+$(function (){
   $('.modal').modal();
+  $('select').formSelect();
+
+
+  $.ajax(
+    {
+        url:"http://localhost:54617/Services.svc/Airoport/API/Data/GetAllFlight",
+        type: "GET",
+        success: function (data){
+
+
+            var i=0;
+            $('#cityfrom').append(`<option value="" disabled selected>Choose your destination</option>`);
+           data.GetAllFlightResult.forEach(element => {
+           i++;
+          $('#cityfrom').append(`<option value="${i}">${element.From}</option>`);
+           });
+        }
+    }
+)
+
+
+
 });

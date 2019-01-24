ff5bc8c4642b8d4dcab867e4e7f2a723b3812c29
Ivan Tymoschuk <ivantymo99@gmail.com>
2019-01-24 13:33:22 +0200

    fixed bugs like Andriy

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -33,23 +33,54 @@ $(function () {
             }
         }
     )
-
-    $("#confirm").click(function () {
-
-
-
-
+    
+    
+    $( "#cityfrom" ).change(function() {
 
         var name = $("#cityfrom option:selected").text();
-
         var id = name.match(/(\d+)/g);
-        var n = $("input[name='group1']:checked + span").text();
         var radioValue = $("input[name='group1']:checked").val();
+        $.ajax(
+            {
+                url: `http://localhost:54617/Services.svc/Airoport/API/Data/GetFlightByID?ID=${id[0]}`,
+
+                type: "GET",
+                            success: function (data) {
+                                $("#time").text(moment(moment(data.GetFlightByIdResult.Date).toDate()));
+                                
+
+                                
+                                $("#places").text(data.GetFlightByIdResult.Count)
 
 
-        $("#main_info").html(`<p><b>${name}</b> <span>  ${n}</span></p>`)
+                                if(radioValue==="1")
+                                $("#price").text(data.GetFlightByIdResult.PriceEconom);
+                                if(radioValue==="2")
+                                $("#price").text(data.GetFlightByIdResult.PriceStandart);
+                                if(radioValue==="3")
+                                $("#price").text(data.GetFlightByIdResult.PriceBussines);
+                                
+                            }
+            }
+        )
+      });
 
-        $("#price").html()
+    $("#confirm").click(function () {
+        
+        
+       
+    var name = $("#cityfrom option:selected").text();
+
+       
+    var n = $("input[name='group1']:checked + span").text();
+    var id = name.match(/(\d+)/g);
+    var radioValue = $("input[name='group1']:checked").val();
+        
+
+
+    $("#main_info").html(`<p><b>${name}</b> <span>  ${n}</span></p>`)
+
+        
         $("#agree").click(function () {
            
 
